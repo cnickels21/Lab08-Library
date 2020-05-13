da3a9821b2d76c8a2161d16cd7ddc79c93b544d3
sihem azibi <s.azibi@live.fr>
2020-05-13 14:38:39 -0500

    library constructor done

---
+++ Lab08_Library/Library.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Lab08_Library
+{
+    public class Library<T>
+    {
+        T[] books;
+        int count;
+
+
+
+        public int Count => count;
+
+        public Library(int capacity)
+        {
+            books = new T[capacity];
+        }
+        public Library() : this(5)
+        {
+
+        }
+
+    }
+}
