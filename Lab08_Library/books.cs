2796d0ea6c2292441c2a8f1367c2dff9c68d164d
sihem azibi <s.azibi@live.fr>
2020-05-13 15:30:30 -0500

    book class

---
+++ Lab08_Library/books.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab08_Library
+{
+    public class Books
+    {
+        public string Title { get; }
+        public string Author { get; }
+        public enum Genra { Fantacy, Science, History,Geography}
+    }
+}
